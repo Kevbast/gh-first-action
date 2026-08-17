fb4333f1a5dac7bfbee933ec1112107534a3c3b4
Kevin Bayas <kevbast936@gmail.com>
2026-08-17 13:49:51 +0200

    added first custom js action

---
+++ .github/workflows/deploy-s3-javascript/index.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec').exec;
+
+function run() {
+  core.notice('Custom Javascript Action to S3 Deployment');
+   
+}
+run();
+
